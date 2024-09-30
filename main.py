da5386a64c2987a746a0a1d45b2e72f72d7ee438
Arshad Munir <arshadmunir1979@yahoo.co.uk>
2024-09-30 21:03:47 +0100

    updated commets

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ def random_color():
     color = (r, g, b)
     return color
 
-########### Challenge 5 - Spirograph ########
-
 def draw_spirograph(size_of_gap):
     for _ in range(int(360 / size_of_gap)):
         tim.color(random_color())
